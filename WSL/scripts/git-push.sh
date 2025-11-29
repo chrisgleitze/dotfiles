f6275330258b866854f5829a58aa8634b61d106d
Christian Gleitze <connectingdotscoding@gmail.com>
2025-11-29 21:47:34 +0100

    automated dev commit

--- WSL/scripts/git-push.sh
+++ WSL/scripts/git-push.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 git add .
-echo 'Enter the commit message:'
-read commitMessage
-git commit -m "$commitMessage"
-echo 'Enter the name of the branch:'
-read branch
+# echo 'Enter the commit message:'
+# read commitMessage
+# git commit -m "$commitMessage"
+git commit -m "automated dev commit"
+# echo 'Enter the name of the branch:'
+# read branch
 git push origin $branch
-read
+popd
